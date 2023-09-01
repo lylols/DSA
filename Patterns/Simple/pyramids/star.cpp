761221767c91d2c70f6ca1e6e53929732cfb36bc
sheenu <sheenushraddha@gmail.com>
2023-09-01 20:05:36 +0530

    patterns

---
+++ Patterns/Simple/pyramids/star.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n,i,j,k;
+    cin >> n;
+    //This is outernloop which loops for rows
+    for(i=1; i<=n; i++){
+        //This inner loop loops for clms
+        for (int j =n; j>=i; j--){
+            cout <<" ";
+        }
+        //for printing stars
+        for(k=1;k<=2*i-1;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
